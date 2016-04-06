6aa1c2502f85118a64f7cefe210fcf415a069476
Bigless27 <lessonryan@gmail.com>
2016-04-06 18:22:42 -0500

    Complete position function

--- Rover.rb
+++ Rover.rb
@@ -1,6 +1,19 @@
 class Rover
-  def initialize
+  attr_accessor :x, :y,:direction
+  def initialize(x,y,direction)
+    @x = x
+    @y = y
+    @direction = direction
+  end
 
+  def position
+    "The Current Position is #{@x}, #{@y}, and facing #{@direction}"
+  end
+
+  def move(coordinates)
   end
 
 end
+
+
+
--- rover_spec.rb
+++ rover_spec.rb
@@ -1,9 +1,13 @@
 require_relative "Rover"
 
 describe "Rover" do
+  let(:newRover){Rover.new(5,4,'N')}
 
   it "has a Rover class" do
-    newRover = Rover.new
     expect(newRover).to be_instance_of(Rover)
+  end
+
+  it "Can report it's own position" do
+    expect(newRover.position).to match("The Current Position is #{newRover.x}, #{newRover.y}, and facing #{newRover.direction}")
   end
 end
