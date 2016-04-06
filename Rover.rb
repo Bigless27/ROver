12f05b3bdec7e2203d7e1b2ec3913539ecc76a54
Bigless27 <lessonryan@gmail.com>
2016-04-06 18:57:19 -0500

    Add move positon method

--- Rover.rb
+++ Rover.rb
@@ -1,19 +1,41 @@
 class Rover
+  attr_reader :compass
   attr_accessor :x, :y,:direction
+
   def initialize(x,y,direction)
     @x = x
     @y = y
     @direction = direction
+    @compass = {'N'=> 1,'E'=> 1,'S'=> -1,'W'=>-1}
   end
 
   def position
-    "The Current Position is #{@x}, #{@y}, and facing #{@direction}"
+    "#{@x}, #{@y}, #{@direction}"
   end
 
   def move(coordinates)
+    coordinates.each_char do |value|
+      if value == 'M'
+        move_position
+      end
+    end
   end
 
+  def move_position
+    if @direction == 'E' || @direction == 'W'
+      self.x +=@compass[@direction]
+    else
+      self.y +=@compass[@direction]
+    end
+  end
+
+  def move_direction
+
+  end
 end
 
+new_rover = Rover.new(3,4,'S')
+new_rover.move("M")
+puts new_rover.position
 
 
--- rover_spec.rb
+++ rover_spec.rb
@@ -1,13 +1,21 @@
 require_relative "Rover"
 
 describe "Rover" do
-  let(:newRover){Rover.new(5,4,'N')}
+  let(:new_rover){Rover.new(5,4,'N')}
 
   it "has a Rover class" do
-    expect(newRover).to be_instance_of(Rover)
+    expect(new_rover).to be_instance_of(Rover)
   end
 
   it "Can report it's own position" do
-    expect(newRover.position).to match("The Current Position is #{newRover.x}, #{newRover.y}, and facing #{newRover.direction}")
+    expect(new_rover.position).to match("#{new_rover.x}, #{new_rover.y}, #{new_rover.direction}")
+  end
+
+  context "The rover can move" do
+    it "the rover can change position" do
+      rover = Rover.new(3,3,'N')
+      rover.move('M')
+      expect(rover.position).to match("3, 4, N")
+    end
   end
 end
